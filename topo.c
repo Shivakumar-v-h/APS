ffbde497d41380d7f9eb7462731d861b65bf292b
Shivakumar-v-h <35775971+Shivakumar-v-h@users.noreply.github.com>
2018-03-07 11:10:23 +0530

    Create topo.c

---
+++ topo.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int top=-1;
+int stack1[6];
+void push(int num)
+{
+    stack1[++top]=num;
+}
+int pop()
+{
+    return (stack1[top--]);
+}
+void topological(int arr[][10],int n)
+{
+    int i,j,k,temp;
+    int t[n+1];
+    int x=0;
+    int *visited=(int *)malloc(sizeof(int)*n);
+    int *in_degree=(int *)malloc(sizeof(int)*n);
+    memset(visited,0,sizeof(int)*n);
+    memset(in_degree,0,sizeof(int)*n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(arr[j][i]==1)
+                in_degree[i]=in_degree[i]+1;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",in_degree[i]);
+    }
+    printf(("\n\n"));
+    for(k=0;k<4;k++)
+    {
+    for(i=0;i<n;i++)
+    {
+        if(in_degree[i]==0)
+        {
+            push(i);
+            visited[i]=1;
+        }
+    }
+    if(top!=-1)
+    {
+        temp=pop();
+        printf("temp=%d\n",temp);
+        t[x++]=temp;
+        for(j=0;j<n;j++)
+        {
+            if(arr[temp][j]==1 && visited[j]==0)
+            {
+                in_degree[j]=in_degree[j]-1;
+                if(in_degree[j]==0)
+                {
+                    push(j);
+                    visited[j]=1;
+                }
+            }
+        }
+    }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d  ",t[i]);
+    }
+}
+int main()
+{
+    int m[10][10] ={{0, 0, 1, 1, 0,0},
+                    {0, 0, 0, 1, 1,0},
+                    {0, 0, 0, 1, 0,1},
+                    {0, 0, 0, 0, 0,1},
+                    {0, 0, 0, 0, 0,1},
+                    {0, 0, 0, 0, 0,0}};
+    int n=6;
+    topological(m,n);
+}
